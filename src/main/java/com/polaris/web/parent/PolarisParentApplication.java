504b576305d7923433e32be7fa31aa2e1e33e4e2
pyjamans <pyjaman_mt@outlook.com>
2020-12-14 22:56:54 +0800

    polaris-parent项目搭建

---
+++ src/main/java/com/polaris/web/parent/PolarisParentApplication.java
@@ -0,0 +1,13 @@
+package com.polaris.web.parent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PolarisParentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PolarisParentApplication.class, args);
+    }
+
+}
